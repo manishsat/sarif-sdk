81a5f61b449bb28c9b3ad16ca9f759d9b6235198
Chris Meyer <v-chrmey@microsoft.com>
2017-12-19 13:34:58 -0800

    Remove Viewer from Everything.sln, reference sarif nuget packages

--- BuildAndTest.cmd
+++ BuildAndTest.cmd
@@ -128,11 +128,6 @@ if "%ERRORLEVEL%" NEQ "0" (
 goto ExitFailed
 )
 
-src\packages\xunit.runner.console.2.3.0\tools\net452\xunit.console.x86.exe bld\bin\Sarif.Viewer.VisualStudio.UnitTests\AnyCPU_%Configuration%\Sarif.Viewer.VisualStudio.UnitTests.dll
-if "%ERRORLEVEL%" NEQ "0" (
-goto ExitFailed
-)
-
 goto Exit
 
 :ExitFailed
